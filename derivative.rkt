475a652497777328731fcb85aee6c09ca605cfe5
Cantong <cantong@Cantongs-Mini.lan>
2019-08-08 16:18:20 +0800

    slope->line

--- derivative.rkt
+++ derivative.rkt
@@ -10,7 +10,16 @@
 
 (plot (list (axes)
             (function position 0 2)
-            (function (points->line (vector 0.9 (position 0.9))
+            (function (points->line (vector 2 (position 2))
                                     (vector 1 (position 1)))
-                      0 2))
+                      0 2
+                      #:label "secant"
+                      #:color 'orange
+                      #:style 'dot)
+            (function (slope->line (vector 1 (position 1))
+                                   30)
+                      0 2
+                      #:label "tangent"
+                      #:color 'green)
+            )
       #:y-min 0)
--- linear.rkt
+++ linear.rkt
@@ -1,17 +1,42 @@
 #lang racket
 
-(provide points->line)
+(provide points->line
+         slope->line
+         slope)
 
 (define point/c (vectorof real?))
 
-(define/contract (points->line p1 p2)
-  (-> point/c point/c procedure?)
+(define/contract (slope p1 p2)
+  (-> point/c point/c real?)
   (let-values ([(x1 y1) (vector->values p1)]
                [(x2 y2) (vector->values p2)]
                )
-    (let ([t (/ (- y2 y1)
-                (- x2 x1))])
+    (/ (- y2 y1)
+       (- x2 x1)))
+  )
+(module+ test
+  (require rackunit rackunit/text-ui)
+
+  (check-equal? (slope #(0 0) #(1 1)) 1)
+  )
+
+(define/contract (points->line p1 p2)
+  (-> point/c point/c procedure?)
+  (let-values ([(x1 y1) (vector->values p1)])
+    (let ([s (slope p1 p2)])
       (lambda (x) (+ y1
-                     (* t
+                     (* s
                         (- x x1))))))
+  )
+
+(define/contract (slope->line p s)
+  (-> point/c real? procedure?)
+  (let-values ([(x0 y0) (vector->values p)])
+    (lambda (x) (+ y0
+                   (* s
+                      (- x x0)))))
+  )
+(module+ test
+  (check-equal? ((points->line #(0 0) #(1 1)) 2) 2)
+  (check-equal? ((slope->line #(0 0) 1) 2) 2)
   )
